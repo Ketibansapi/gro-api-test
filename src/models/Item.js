82a3ddb51c3f226ec4e09e9865f6f1339ed90502
Deary <dearyherdiano@gmail.com>
2019-11-25 19:57:34 +0800

    Modify itemSchema by maker_id

--- src/models/Item.js
+++ src/models/Item.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const itemSchema = new mongoose.Schema({
   name: String,
   brand: String,
   price: String,
   quantity: Number,
-  services: {
-    type: Map,
-    of: String
-  }
+  // services: {
+  //   type: Map,
+  //   of: String
+  // }
 })
 
 module.exports = mongoose.model('Item', itemSchema)
